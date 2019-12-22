9432f3daff4986332f75ae7f38067c0a2e9dcf6f
tribbianii <tribbiani108@gmail.com>
2019-12-21 22:52:24 -0600

    delete specific tree node

---
+++ src/leetcode/TreeDeleteNode.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import leetcode.Tree.TreeNode;
+
+class TreeDeleteNode {
+    public TreeNode delete (TreeNode root, int target) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val > target) {
+            root.left = delete (root.left, target);
+            return root;
+        }
+        else if (root.val < target) {
+            root.right = delete (root.right, target);
+            return root;
+        }
+        else {
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            if (root.left == null || root.right == null) {
+                return root.left == null ? root.right : root.left;
+            }
+            int newRoot = findSmallest (root.right);
+            root.val = newRoot;
+            root.right = delete (root.right, newRoot);
+            return root;
+        }
+    }
+    public int findSmallest (TreeNode root) {
+        int min = 0;
+        while (root != null) {
+            min = root.val;
+            root = root.left;
+        }
+        return min;
+    }
+}
